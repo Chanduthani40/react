918ba169792fae2ea5a902160d8afec1ab388b6b
Chanduthani40 <92813267+Chanduthani40@users.noreply.github.com>
2022-11-23 15:27:06 +0530

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>deeksitha</li>
+      <li>deekshitha</li>
+      <li>deekshitha</li>
+    </ul>
+  );
+}
+export default List;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import Heading from "./Heading";
+//import List from "./List";
+import App from "./components/App";
+
+const customStyle = {
+  color: ""
+};
+
+ReactDOM.render(<App />, document.getElementById("demo"));
